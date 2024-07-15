d8555d47e453686fef8622a7339aadf9714863d3
JoaoDev00 <poderosogud@gmail.com>
2024-07-15 16:36:54 -0300

    Mais exercicios
    MORE MORE MORE!!

---
+++ Aula 4/condicionais.js
@@ -0,0 +1,23 @@
+// Se o horario for menor que 12:00, diga bom diga
+// Se não, diga Boa tarde
+
+// let hora = 10 ;
+
+
+// if(hora < 12) {
+
+//     console.log("Bom dia! ");
+// }
+// // ===============================================
+
+// let saborPizza;
+// if (saborPizza ==" calabresa") {
+//     console.log("Saindo uma pizza de calabresa");   
+// }
+//     else if(saborPizza == "frango") {
+//     }
+//     else if (saborPizza == "Queijo") {   
+//     }
+//     else if (saborPizza == "Saindo uma pizza de queijo") {
+        
+//     }
