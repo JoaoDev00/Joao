a270b89a0a061eee1015e769b45f674814257316
João Victor <poderosogud@gmail.com>
2024-08-12 16:44:36 -0300

    OAOAOOA
    OHHOHOH

---
+++ Aula_9/Perfeito.js
@@ -0,0 +1,17 @@
+// let conta = {
+//   saldo: 0,
+//   historico: [],
+//   saquesDiarios: 0
+// };
+
+// let opcao;
+
+// while (true) {
+//   console.log("Selecione uma opção:");
+//   console.log("1. Depositar");
+//   console.log("2. Sacar");
+//   console.log("3. Extrato");
+//   console.log("4. Sair");
+
+//   opcao = prompt("Digite a opção: ");
+
