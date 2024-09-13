65a9645ae4b1ab77113ecd252b84c7d5e10e1a40
João Victor <poderosogud@gmail.com>
2024-09-13 16:41:13 -0300

    HHAHAHA
    HAHAHAHAHHAHA

---
+++ aula_04/É isso/src/planoDeServiço.java
@@ -0,0 +1,130 @@
+
+
+
+// Construir um programa para uma empresa de serviços. O operador irá digitar o salário de um cliente. Se o salário for maior ou igual que R$ 3.000,00, 
+// o programa irá escrever na tela “Oferecer Plano Alfa” e, caso seja menor, o programa escreverá “Oferecer Plano Beta”.
+
+
+
+// import java.util.*;
+
+// public class planoDeServiço{
+//     public static void main(String[] args) {
+//         Scanner entrada = new Scanner(System.in);
+
+//         System.out.println("Digite o seu nome: ");
+//         String nome = entrada.nextLine();
+//         System.out.println("Digite o seu salario: ");
+//         double money = entrada.nextDouble();
+
+//         System.out.println("Nome: " + nome);
+//         System.out.println("Salario R$:" + money);
+
+//         if (money >= 3000.00) {
+//             System.out.println("Oferecer Plano Alfa");
+//         } else {
+//             System.out.println("Oferecer Plano Beta");
+//         }
+
+//         entrada.close();
+//     }
+// }
+
+
+// ==========================================================================//==============================================================================
+
+// Construir um programa onde um vendedor irá digitar quanto ele vendeu em um mês na loja. 
+// Caso esse total de vendas seja maior ou igual a R$ 10.000, 
+// o programa irá calcular uma comissão de 4% sobre suas vendas, 
+// mas caso a venda seja menor, o programa calculará apenas 2%.
+
+
+
+
+// import java.util.*;
+
+// public class planoDeServiço{
+//     public static void main(String[] args) {
+//         Scanner entrada = new Scanner(System.in);
+
+//         System.out.println("Digite o mês: ");
+//         String nome = entrada.nextLine();
+//         System.out.println("Digite o valor: ");
+//         double money = entrada.nextDouble();
+//         entrada.nextLine(); // 
+
+//         System.out.println("Mês: " + nome);
+//         System.out.println("Valor R$: " + money);
+
+//         double taxaBasica = 0.0; 
+//         if (money >= 10000.00) {
+//             double total = money * 0.04; 
+//             System.out.printf("Valor da conta = %.2f%n", (money + total));
+//         } else {
+//             System.out.printf("Valor da conta = %.2f%n", money);
+//         }
+
+//         entrada.close();
+//     }
+// }
+
+
+// ==========================================================================//==============================================================================
+
+// O usuário de uma página de games deverá digitar sua pontuação. 
+// Caso digite um valor acima de 1.000, 
+// o programa retornará o texto “Usuário VIP” ou, caso contrário, 
+// “Aumente sua pontuação para se tornar um usuário VIP”.
+
+
+
+
+// import java.util.*;
+
+// public class planoDeServiço{
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("Digite sua pontuação: ");
+//         int pontuacao = scanner.nextInt();
+
+//         if (pontuacao >= 1000) {
+//             System.out.println("Usuario vip");
+//         } else {
+//             System.out.println("Aumente sua pontuação para se tornar um usuário VIP");
+//         }
+
+//         scanner.close();
+//     }
+// }
+
+
+
+
+// ==========================================================================//==============================================================================
+
+
+
+
+        // Scanner entrada = new Scanner(System.in);
+
+        // System.out.print("Digite a quantidade de números:");
+        // int n = entrada.nextInt();
+        // int soma = 0;
+
+        
+        
+        // for (int i = 0; i < n ; i++ ){
+        //     System.out.print("Informe um número: ");
+        //     int x = entrada.nextInt();
+        // }
+
+   
+
+
+// String senha  = "12345";
+        // if (senha.equals("12345")) {
+        //     System.out.println("Acesso Autorizado!");
+        // } else{
+        //     System.out.println("Senha, incorreta!");
+        // }
